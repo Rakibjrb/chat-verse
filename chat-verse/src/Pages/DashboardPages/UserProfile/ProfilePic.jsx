768ad1413adffa13dcd36e88fd74882f28a6f299
Rakibjrb <rakibjrb572157@gmail.com>
2024-04-05 16:05:15 +0600

    created profile pic and cover photo pic shape with responsive

--- chat-verse/src/Pages/DashboardPages/UserProfile/ProfilePic.jsx
+++ chat-verse/src/Pages/DashboardPages/UserProfile/ProfilePic.jsx
@@ -1,5 +1,22 @@
+import { FaCamera } from "react-icons/fa";
+
 const ProfilePic = () => {
-  return <div>Profile Pics</div>;
+  return (
+    <div className="relative">
+      <div className="relative w-full h-[140px] md:h-[220px] bg-slate-200 rounded-lg">
+        <div className="absolute bottom-5 right-3 flex justify-center items-center p-3 bg-white rounded-full">
+          <FaCamera className="text-xl" />
+        </div>
+      </div>
+      <div className="absolute top-16 md:top-24 left-4 md:left-6 w-1/3 md:w-1/4 lg:w-1/5 h-auto bg-slate-400 rounded-full border-[15px] border-white">
+        <img
+          className="rounded-full"
+          src="https://i.ibb.co/5x441PC/user.png"
+          alt="profile image"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default ProfilePic;
--- chat-verse/src/Pages/DashboardPages/UserProfile/UserProfile.jsx
+++ chat-verse/src/Pages/DashboardPages/UserProfile/UserProfile.jsx
@@ -7,7 +7,7 @@ const UserProfile = () => {
       <div className="hidden xl:block lg:w-1/4 p-3 bg-white rounded-lg">
         <ProfileCard />
       </div>
-      <div className="w-full xl:w-3/4 p-3 bg-white">
+      <div className="w-full xl:w-3/4 p-3">
         <ProfilePic />
       </div>
     </div>
